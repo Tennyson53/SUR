2e98dd1adb92c606c3f21e2159fe52dda5fde1fb
Tennyson_Z <tennyson0503@hotmail.com>
2015-08-31 14:31:13 +0800

    modify senlin cluster_function file

--- magnum/sur/cluster_function.py
+++ magnum/sur/cluster_function.py
@@ -21,25 +21,28 @@ def create_cluster(OSC):
     sc = OSC.senlin()
 
     # Create Profile
-    pr = Profile.profile_create(sc, 'SUR_Profile', 'os.nova.server',
-                                '/opt/stack/magnum/magnum/sur/SURspec/SUR.spec',
+    pr_master = Profile.profile_create(sc, 'SUR_Master_Profile', 'os.nova.server',
+                                '/opt/stack/magnum/magnum/sur/SURspec/SUR_master.spec',
+                                '1111')
+    pr_minion = Profile.profile_create(sc, 'SUR_Minion_Profile', 'os.nova.server',
+                                '/opt/stack/magnum/magnum/sur/SURspec/SUR_minion.spec',
                                 '1111')
     time.sleep(1)
     LOG.info(pr)
     
     # Create Cluster
-    cr = Cluster.cluster_create(sc, 'SUR_Cluster', 'SUR_Profile')
+    cr = Cluster.cluster_create(sc, 'SUR_Cluster', 'SUR_Minion_Profile')
     time.sleep(1)
     LOG.info(cr)
 
     # Create Nodes
     nr_master = Node.node_create(sc, 'SUR_Node_Master', 'SUR_Cluster',
-                                 'SUR_Profile')
+                                 'SUR_Master_Profile')
     time.sleep(1)
     LOG.info(nr_master)
 
     nr_minion = Node.node_create(sc, 'SUR_Node_Minion', 'SUR_Cluster',
-                                 'SUR_Profile')
+                                 'SUR_Minion_Profile')
     time.sleep(1)
     LOG.info(nr_minion)
 
